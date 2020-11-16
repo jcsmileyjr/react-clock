a428a8c41feacafe53d4804071bb6c9d6480166f
Smiley <jcsmileyjr@gmail.com>
2020-11-16 16:04:27 -0600

    Show the hands

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-style">
+      <main id="clock-face">
+        <section id="hour-hand" className="hand"></section>
+        <section id="minute-hand" className="hand"></section>
+        <section id="second-hand" className="hand"></section>
+      </main>
     </div>
   );
 }
