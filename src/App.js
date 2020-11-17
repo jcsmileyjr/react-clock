c003252fba4696d65818a786136e7ea7105f67fc
Smiley <jcsmileyjr@gmail.com>
2020-11-17 03:59:56 -0600

    Update functionality for minutes and hour hand movement around the clock

--- src/App.js
+++ src/App.js
@@ -3,24 +3,48 @@ import React, {useState} from 'react';
 
 function App() {
   const [secondHandRotation, setSecondhandRotation] = useState(90);
+  const [minuteHandRotation, setMinuteHandRotation] = useState(90);
+  const [hourHandRotation, setHourHandRotation] = useState(90);
+  const [digitalTime, setDigitalTime] = useState(0);
 
+  /*
+  **Calculate the degrees to point each hand based on current seconds/minutes/hour & total degrees 
+    in a circle and the original 90 degree angle (12 o'clock).
+  */  
   const setDate = () => {
     const now = new Date();
     const seconds = now.getSeconds(); 
-    /*Calculate the degrees to point the hand based on seconds & total degrees 
-    in a circle and the original 90 degree angle (12 o'clock)*/   
+     
+    // Calculate teh current degree of the second hand by using the current second of the minute
     const updatedSecondHandDegrees = ((seconds/60) * 360) + 90;
     setSecondhandRotation(updatedSecondHandDegrees);
-    console.log(seconds);
+
+    // Calcuate the current degree of the minute hand by using the current minute of the hour
+    const minutes = now.getMinutes();
+    const updatedMinuteHandDegrees = ((minutes/60) * 360) + 90;
+    setMinuteHandRotation(updatedMinuteHandDegrees);
+
+    // Calcuate the current degree of the hour hand by using the current minute of the hour
+    const hours = now.getHours();
+    const updateHourHandDegrees = ((hours/12) *360) + 90;
+    setHourHandRotation(updateHourHandDegrees);
+
+    // Calcuate the current digital time
+    let test = now.toLocaleTimeString();
+    setDigitalTime(test);
   }
 
-  setInterval(setDate, 1000)
+  setInterval(setDate, 1000) 
+
   return (
     <div className="page-style">
+      <article id="digital-time">
+        <h1>{digitalTime}</h1>
+      </article>
       <main id="clock-face">
-        <section id="hour-hand" className="hand" ></section>
-        <section id="minute-hand" className="hand"></section>
-        <section id="second-hand" className="hand"style={{transform:`rotate(${secondHandRotation}deg)`}}></section>
+        <section id="hour-hand" className="hand" style={{transform:`rotate(${hourHandRotation}deg)`}} ></section>
+        <section id="minute-hand" className="hand" style={{transform:`rotate(${minuteHandRotation}deg)`}}></section>
+        <section id="second-hand" className="hand" style={{transform:`rotate(${secondHandRotation}deg)`}}></section>
       </main>
     </div>
   );
